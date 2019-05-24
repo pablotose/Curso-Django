d43b28fddac3893ebeddf33f0e720fba94125a01
pablotose <50028288+pablotose@users.noreply.github.com>
2019-05-24 21:05:41 +0200

    Commit 14
    Video nº 17

--- src/boletin/forms.py
+++ src/boletin/forms.py
@@ -7,6 +7,19 @@ class RegModelForm(forms.ModelForm):
 		model = Registrado
 		fields = ["nombre", "email"]
 
+	def clean_email(self):
+		email = self.cleaned_data.get("email")
+		email_base, proveedor = email.split("@")
+		dominio, extension = proveedor.split(".")
+		if not extension == "edu":
+			raise forms.ValidationError("Por favor utiliza un email con la extension .EDU")
+		return email
+
+	def clean_nombre(self):
+		nombre = self.cleaned_data.get("nombre")
+		#validaciones
+		return nombre
+
 class RegForm(forms.Form):
 	nombre = forms.CharField(max_length=100)
 	email = forms.EmailField()
