5c4b1b459b32d6559e8c893e56bc9e29f7b0bc8e
pablotose <50028288+pablotose@users.noreply.github.com>
2019-05-24 21:18:25 +0200

    Commit 15
    Video nº 18

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -5,6 +5,9 @@ from .models import Registrado
 
 # Create your views here.
 def inicio(request):
+	titulo = "HOLA"
+	if request.user.is_authenticated():
+		titulo = "Bienvenido %s" %(request.user)
 	form = RegForm(request.POST or None)
 	if form.is_valid():
 		form_data = form.cleaned_data
@@ -12,6 +15,7 @@ def inicio(request):
 		abc2 = form_data.get("nombre")
 		obj = Registrado.objects.create(email=abc, nombre=abc2)
 	context = {
+		"titulo": titulo,
 		"el_form": form,
 	}
 	return render(request, "inicio.html", context)
