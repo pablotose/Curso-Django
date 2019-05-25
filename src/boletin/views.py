8f6952a40e3b4f048c7dcdccd1a76151c7b1be07
pablotose <50028288+pablotose@users.noreply.github.com>
2019-05-25 19:40:49 +0200

    Commit 16
    Video nº 19

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .forms import RegForm
+from .forms import RegForm, RegModelForm
 from .models import Registrado
 
 # Create your views here.
@@ -8,14 +8,38 @@ def inicio(request):
 	titulo = "HOLA"
 	if request.user.is_authenticated():
 		titulo = "Bienvenido %s" %(request.user)
-	form = RegForm(request.POST or None)
-	if form.is_valid():
-		form_data = form.cleaned_data
-		abc = form_data.get("email")
-		abc2 = form_data.get("nombre")
-		obj = Registrado.objects.create(email=abc, nombre=abc2)
+	form = RegModelForm(request.POST or None)
+
 	context = {
-		"titulo": titulo,
-		"el_form": form,
-	}
+			"titulo": titulo,
+			"el_form": form,
+		}
+
+	if form.is_valid():
+		instance = form.save(commit=False)
+		nombre = form.cleaned_data.get("nombre")
+		email = form.cleaned_data.get("email")
+		if not instance.nombre:
+			instance.nombre = "PERSONA"
+		instance.save()
+
+		context = {
+			"titulo": "Gracias %s!" %(nombre)
+
+		}
+
+		if not nombre:
+			context = {
+				"titulo": "Gracias %s!" %(email)
+			}
+
+		print instance
+		print instance.timestamp
+		#form_data = form.cleaned_data
+		#abc = form_data.get("email")
+		#abc2 = form_data.get("nombre")
+		#obj = Registrado.objects.create(email=abc, nombre=abc2)
+
+
+
 	return render(request, "inicio.html", context)
