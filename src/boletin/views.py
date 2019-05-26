09e75f226904798eaaf6773e7bd878abe83d7358
pablotose <50028288+pablotose@users.noreply.github.com>
2019-05-26 14:00:46 +0200

    Commit 20
    Video nº 23

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -44,7 +44,7 @@ def inicio(request):
 
 
 
-	return render(request, "inicio.html", context)
+	return render(request, "base.html", context)
 
 def contact(request):
 	form = ContactForm(request.POST or None)
--- src/pd110/settings.py
+++ src/pd110/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = '/media/'
+MEDIA_URL = '/media/'
 #/static/imagenes/img1.jpg
 
 STATICFILES_DIRS = [
