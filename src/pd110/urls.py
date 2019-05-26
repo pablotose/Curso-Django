093f36563b140078c9a9ddfcaaf6ab170ea34249
pablotose <50028288+pablotose@users.noreply.github.com>
2019-05-26 13:05:44 +0200

    Commit 19
    Video nº 22

--- src/pd110/urls.py
+++ src/pd110/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from boletin import views
@@ -24,3 +26,7 @@ urlpatterns = [
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^$', views.inicio, name='inicio'),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
